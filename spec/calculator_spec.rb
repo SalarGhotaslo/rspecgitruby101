29053e75e482d34f0cad79e96804b9b01603f503
Richard Ewin-Lambert <rewinlambert@deloitte.co.uk>
2021-01-03 12:30:39 +0000

    Passing test for adds 2 integers test

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,4 +7,10 @@ describe Calculator do
     it 'calculates integers' do 
         expect(calculator).to be_instance_of Calculator
     end 
+
+    # create a method that adds 2 integers 
+
+    it 'adds 2 integers' do 
+        expect(calculator.add(5, 5)).to eq 10
+    end 
 end 
