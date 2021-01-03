73842d87ae347d7b355ae5332fdad7c3001d90e1
Salar Ghotaslo <Salar_@live.co.uk>
2021-01-03 14:25:48 +0000

    calculator completed

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -13,4 +13,20 @@ describe Calculator do
     it 'adds 2 integers' do 
         expect(calculator.add(5, 5)).to eq 10
     end 
+    # create a method that subtracts 2 integers 
+
+    it 'subtracts 2 integers' do 
+        expect(calculator.subtract(5, 5)).to eq 0
+    end 
+    # create a method that multiplies 2 integers 
+
+    it 'multiply 2 integers' do 
+        expect(calculator.multiply(5, 5)).to eq 25
+    end 
+    # create a method that divides 2 integers 
+
+    it 'divide 2 integers' do 
+        expect(calculator.divide(5, 5)).to eq 1
+    end 
+
 end 
